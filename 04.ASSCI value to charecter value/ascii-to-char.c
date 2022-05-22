549a39af204dcff8646a78f76d6d401a81890a3e
SalmannFarshee <salmanzihad9024@gmail.com>
2022-05-22 23:49:57 +0600

    iconverted a inteser ascii value to a charecter typer ascii value

---
+++ 04.ASSCI value to charecter value/ascii-to-char.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int asciiVaalue; //a inteser ascii value
+    printf("Type your ASCII value : ");
+    scanf("%d", &asciiVaalue); // we store a value as a inteser
+    printf("The Charecter is : %c", asciiVaalue); // we print the value as a charecter and we are done
+
+    return 0;
+}
