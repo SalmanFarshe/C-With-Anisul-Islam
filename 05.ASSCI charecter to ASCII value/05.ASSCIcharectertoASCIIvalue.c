3692140e56adf658eea46fbb418c2d15a7762a8b
SalmannFarshee <salmanzihad9024@gmail.com>
2022-05-23 20:05:20 +0600

    ascii charecter to ascii value

---
+++ 05.ASSCI charecter to ASCII value/05.ASSCIcharectertoASCIIvalue.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter your charecter : ");
+    scanf("%c", &ch);
+    printf("the value of your given charecter is : %d", ch);
+    return 0;
+}
