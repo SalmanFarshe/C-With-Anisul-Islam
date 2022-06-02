74f29fb3cb797a694dbd810f24d4bcf11528f6e6
SalmannFarshee <salmanzihad9024@gmail.com>
2022-06-02 09:09:22 +0600

    just nothing chng

---
+++ 11.!stMathmeticalExpretion/mathmetical-expretion.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int sum;
+    sum = 4 / 2 + 8 * 4 - (5 + 2) % 3; //just a mathmeticalexpretion nothing else
+    printf("the sumation is %d", sum);
+    return 0;
+}
