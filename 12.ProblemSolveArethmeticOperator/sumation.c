896712f855f74bcbad14d08948431338421fc140
SalmannFarshee <salmanzihad9024@gmail.com>
2022-06-04 07:51:52 +0600

    updated

---
+++ 12.ProblemSolveArethmeticOperator/sumation.c
@@ -0,0 +1,12 @@
+// write a programme which can sum two number
+#include<stdio.h>
+int main(){
+    float FstNmbr, ScndNmbr, Result;
+    printf("Enter First Number : ");
+    scanf("%f", &FstNmbr);
+    printf("Enter Second Number : ");
+    scanf("%f", &ScndNmbr);
+    Result = FstNmbr + ScndNmbr; // sumation of the two nomber
+    printf("The Total Amount is = %.2f", Result);
+    return 0;
+}
