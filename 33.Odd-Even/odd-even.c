25cefbc5424eade0c2b67947a24dc5319c51beec
SalmannFarshee <salmanzihad9024@gmail.com>
2022-09-08 23:42:04 +0600

    odd even number

--- 33.Odd-Even/odd-even.c
+++ 33.Odd-Even/odd-even.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(){
     int num;
-    printf("")
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    if(num % 2 == 0){
+        printf("The given Number is an Even number");
+    }else{
+        printf("The given Number is an Odd number");
+    }
 }
